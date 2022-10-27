082c8caa8a0eb33d3292a2aaa99243c579210737
Sayan kar <sayankar1308@gmail.com>
2022-10-27 20:02:12 +0000

    added routes

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,28 +1,30 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-import CanvasGrid from './components/CanvasGrid';
-import CanvasRoom from './components/CanvasRoom';
-import HomePage from './components/HomePage';
-import Navbar from './components/Navbar';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import DisplayCard from './components/DisplayCard';
-import CardList from './components/CardList';
-
+import CanvasGrid from "./components/CanvasGrid";
+import CanvasRoom from "./components/CanvasRoom";
+import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+import CreateRoom from "./components/CreateRoom";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import DisplayCard from "./components/DisplayCard";
+import CardList from "./components/CardList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App" 
-    // style={{display: "flex", justifyContent: "center", padding: "40px"}}
-    >
-      {/* <CanvasGrid rows={32} columns={32}/> */}
-
-      <Navbar />
-      <HomePage />
-
+    <div className="App">
+      <BrowserRouter >
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/canvas/:canvasId" element={<CanvasRoom />} />
+        <Route path="/create" element={<CreateRoom/>}/>        
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
